05dba25715f556d7af73e55f02ccafea022bfeef
Andros Fenollosa <andros@fenollosa.email>
2020-07-13 20:15:12 +0200

    Fix bug exists thumbnail

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject video-optimize "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject video-optimize "1.0.0-SNAPSHOT"
+  :description "Watcher and optimize videos"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [hawk "0.2.11"]]
   :main ^:skip-aot video-optimize.core
+  :aot  [video-optimize.core]
   :repl-options {:init-ns video-optimize.core})
--- src/video_optimize/core.clj
+++ src/video_optimize/core.clj
@@ -13,11 +13,12 @@
   (hawk/watch! [{:paths   [PATH_VIDEOS]
                  :handler (fn [ctx e]
                             (let [path_raw       (.getAbsolutePath (:file e))
-                                  is_thumbnail   (re-find (re-pattern EXTENSION_THUMBNAIL) path_raw)
+                                  is_thumbnail   (doall (re-find (re-pattern EXTENSION_THUMBNAIL) path_raw))
                                   path_thumbnail (str/join (concat (drop-last (str/split path_raw #"\.")) EXTENSION_THUMBNAIL))]
-                              (if (and (.exists (io/file path_raw)) (not is_thumbnail))
+                              (if (and (.exists (io/file path_raw)) (not is_thumbnail) (not (.exists (io/file path_thumbnail))))
                                 (do
                                   (prn (str "Optimizing: " path_raw))
+                                        ; Optimizing with ffmpeg
                                   (shell/sh "ffmpeg" "-y" "-i" path_raw "-vf" "scale=600:-2" "-c:v" "libx264" "-crf" "23" "-profile:v" "high" "-pix_fmt" "yuv420p" "-color_primaries" "1" "-color_trc" "1" "-colorspace" "1" "-movflags" "+faststart" "-an" "-acodec" "aac" "-ab" "128kb" path_thumbnail)
                                   (prn (str "Finish: " path_thumbnail))))
                               )
